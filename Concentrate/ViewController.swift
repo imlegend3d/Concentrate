c674d9d1a63fda0f68e75371276f866cf0555177
David <david@davids-macbook-pro.local>
2018-11-13 23:51:44 -0500

    Initial Commit

---
+++ Concentrate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentrate
+//
+//  Created by David on 2018-11-13.
+//  Copyright © 2018 David. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
